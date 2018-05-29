07fedf632bf1c4e4ba01af42689f0899221eb5af
dashuncel <korovkinada@ya.ru>
2018-05-29 19:51:32 +0800

    v0

---
+++ main.py
@@ -0,0 +1,36 @@
+# класс животные
+class Animal:
+  pass
+
+# летающие 
+class Flying:
+  pass
+
+# Корова
+class Cow(Animal):
+  pass
+
+# коза
+class Goat(Animal):
+  pass
+
+# свинья
+class pig(Animal):
+  pass
+
+# Овца
+class Sheep(Animal):
+  pass
+
+# курица
+class Hen(Animal):
+  pass
+
+# утка
+class Duck(Animal, Flying):
+  pass
+
+# гусь
+class Goose(Animal, Flying):
+  pass
+ 
